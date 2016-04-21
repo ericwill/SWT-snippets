e82e56d027ee5f70889cb42a9a5f8b7442bb924a
Eric Williams <ewilliams@vmail.me>
2016-04-21 13:21:27 -0400

    Bug 478447: Hyperlink hovering draws a long string over composite
    NOTE: this snippet requires an org.eclipse.ui.forms dependency.
    
    Signed-off-by: Eric Williams <ewilliams@vmail.me>

---
+++ src/testsnippets/Bug478447_HyperlinkRenderingTest.java
@@ -0,0 +1,57 @@
+package testsnippets;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.SashForm;
+import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.forms.widgets.FormText;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+
+public class Bug478447_HyperlinkRenderingTest {
+	public static void main(String[] args) {
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		shell.setLayout(new FillLayout());
+
+		SashForm sashForm = new SashForm(shell, SWT.HORIZONTAL);
+		ScrolledComposite sc = new ScrolledComposite(sashForm, SWT.H_SCROLL | SWT.V_SCROLL);
+		Composite composite = new Composite(sc, SWT.NONE);
+		composite.setLayout(new FillLayout());
+
+		FormToolkit formToolkit = new FormToolkit(display);
+		FormText formText = formToolkit.createFormText(composite, true);
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("<form>");
+		buffer.append("<li>");
+		buffer.append("<a href=\"google.com\">");
+		buffer.append("Some_long_long_long_long_long_long_long_long_long_long_string");
+		buffer.append("</a>");
+		buffer.append("</li>");
+		buffer.append("</form>");
+
+		formText.setText(buffer.toString(), true, false);
+
+		sc.setContent(composite);
+		sc.setMinSize(400, 30);
+		sc.setExpandHorizontal(true);
+		sc.setExpandVertical(true);
+
+		StyledText styledText = new StyledText(sashForm, SWT.NONE);
+		styledText.setText("Right Panel");
+
+		shell.setSize(600, 120);
+		shell.open();
+
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+
+		display.dispose();
+	}
+}
