2bccc8a2bbda7b43ee842623d0db86d311bbad81
lufimtse <lufimtse@unused-10-15-17-78.yyz.redhat.com>
2016-07-04 11:33:47 -0400

    Mylyn icons added.
    Change-Id: I0b2b82d630102ef5fc8237a0b5cd809110b95570
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=489025
    Signed-off-by: lufimtse <lufimtse@unused-10-15-17-78.yyz.redhat.com>

--- src/testsnippets/Test.java
+++ src/testsnippets/Test.java
@@ -2,6 +2,9 @@ package testsnippets;
 
 import java.lang.management.ManagementFactory;
 
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.internal.gtk.OS;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
@@ -18,8 +21,20 @@ public class Test {
 	    shell.setText(System.getProperty("sun.java.command") + " " +  Test.getGtkVersion());
 	}
 	
+	static public void fillImage(Image image, int swtSystemColor) {
+		GC gc = new GC(image);
+		gc.setBackground(Display.getCurrent().getSystemColor(swtSystemColor));
+		gc.fillRectangle(0, 0, 24, 24);
+		gc.dispose();
+	}
 	
-	public void displayLoop() {
+	Test setShellFullScreen() {
+		Rectangle rect = this.display.getBounds();
+		this.shell.setBounds(rect);
+		return this;
+	}
+	
+	void displayLoop() {
  	    shell.open();
 	    while (!shell.isDisposed ()) {
 	        if (!display.readAndDispatch ()) display.sleep ();
@@ -28,7 +43,7 @@ public class Test {
 	}
 	
 	
-	public static String getGtkVersion () {
+	static String getGtkVersion () {
 		return "Gtk: " + OS.gtk_major_version() + "." + OS.gtk_minor_version() + "." + OS.gtk_micro_version() + "  pid:"
 				+ ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
 	}
