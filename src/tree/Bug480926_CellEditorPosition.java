27c39ef79c159eacafd44e171a5ab1519f2ab9df
Eric Williams <ewilliams@vmail.me>
2016-09-19 20:38:22 -0400

    Added snippet for bug 480296.
    Signed-off-by: Eric Williams <ewilliams@vmail.me>

---
+++ src/tree/Bug480926_CellEditorPosition.java
@@ -0,0 +1,61 @@
+package tree;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.TreeEditor;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.*;
+
+public class Bug480926_CellEditorPosition {
+    public static void main(String[] args) {
+        Display display = new Display();
+        Shell shell = new Shell(display);
+        shell.setLayout(new FillLayout());
+
+        final Tree tree = new Tree(shell, SWT.FULL_SELECTION);
+        tree.setLinesVisible(true);
+        tree.setHeaderVisible(true);
+
+        TreeColumn column1 = new TreeColumn(tree, 0);
+        column1.setText("Column 1");
+        column1.setWidth(150);
+
+        TreeColumn column2 = new TreeColumn(tree, 0);
+        column2.setText("Column 2");
+        column2.setWidth(150);
+
+        TreeItem item = new TreeItem(tree, SWT.NONE, 0);
+        item.setText("Row 1");
+
+        final TreeEditor treeEditor = new TreeEditor(tree);
+        treeEditor.horizontalAlignment = SWT.LEFT;
+        treeEditor.grabHorizontal = true;
+
+        tree.addMouseListener(new MouseAdapter() {
+            public void mouseUp(MouseEvent e) {
+                TreeItem item = tree.getSelection()[0];
+                Text text = new Text(tree, SWT.NONE);
+                treeEditor.setEditor(text, item, 1);
+            }
+        });
+
+        tree.addListener(SWT.PaintItem, new Listener() {
+            @Override
+            public void handleEvent(Event event) {
+
+            }
+        });
+
+        shell.setSize(600, 400);
+        shell.open();
+
+        while (!shell.isDisposed()) {
+            if (!display.readAndDispatch()) {
+                display.sleep();
+            }
+        }
+
+        display.dispose();
+    }
+}
