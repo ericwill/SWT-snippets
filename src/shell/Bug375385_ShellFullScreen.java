ca9c7c4490d17c272a1aa6c65e1d260547ba98b4
Eric Williams <ewilliams@vmail.me>
2016-09-19 20:45:31 -0400

    Added snippet for bug 375385.
    Signed-off-by: Eric Williams <ewilliams@vmail.me>

---
+++ src/shell/Bug375385_ShellFullScreen.java
@@ -0,0 +1,60 @@
+package shell;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class Bug375385_ShellFullScreen
+{
+    public static void main(String[] args)
+    {
+        Display display = new Display();
+        
+        Shell parentShell = new Shell(display);
+        parentShell.setText("Parent shell");
+        parentShell.setSize(400, 300);
+                
+        final Shell childShell = new Shell(parentShell, SWT.APPLICATION_MODAL | SWT.RESIZE | SWT.CLOSE);
+        childShell.setSize(300, 200);
+        childShell.setText("Child shell");        
+        
+        childShell.addMouseListener(new MouseListener()
+        {   
+            @Override
+            public void mouseUp(MouseEvent event)
+            {   
+                
+            }
+            
+            @Override
+            public void mouseDown(MouseEvent event)
+            {
+                
+            }
+            
+            @Override
+            public void mouseDoubleClick(MouseEvent event)
+            {
+                // Toggle full screen mode: works on Mac and Windows, won't work on Linux
+                System.out.println("Full screen: " + childShell.getFullScreen());
+                
+                childShell.setFullScreen(! childShell.getFullScreen());
+                
+                System.out.println("Full screen: " + childShell.getFullScreen());                
+            }
+        });
+        
+        parentShell.open();
+        childShell.open();
+                
+        while (! parentShell.isDisposed())
+        {
+            if (! display.readAndDispatch())
+            {
+                display.sleep();
+            }
+        }
+        display.dispose();
+    }    
+}
